6b9cc228a1793619dfda427e18518c4c030a1193
JaxonWright <jaxonhwright@gmail.com>
2020-01-22 22:06:36 -0500

    Routing for new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashComponent } from './pages/dash/dash.component';
 import { AboutComponent } from './pages/about/about.component';
+import { DonateComponent } from './pages/donate/donate.component';
 
 
 const routes: Routes = [
   { path: '', component: DashComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'donate', component: DonateComponent },
   { path: '**', redirectTo: '' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { LogoComponent } from './elements/logo/logo.component';
 import { CardComponent } from './elements/card/card.component';
+import { DonateComponent } from './pages/donate/donate.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { CardComponent } from './elements/card/card.component';
     FooterComponent,
     LogoComponent,
     CardComponent,
+    DonateComponent,
   ],
   imports: [
     BrowserModule,

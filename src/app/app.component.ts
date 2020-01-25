c398ac3e213d3a592176c3aac40daf8352613d69
JaxonWright <jaxonhwright@gmail.com>
2020-01-24 22:14:49 -0500

    Automatically adjust zoom when fullscreening.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,13 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class AppComponent {
   title = 'testube';
   teslaUserAgentRegEx = /Tesla\/([0-9]{4}.[0-9]{2}.[0-9]{2}.[0-9]{1,2})-(.{7})/g;
-
+  isFullscreen : boolean;
+  theaterZoomLevel : number = 0.64; // make size 64% of original size to account for ViewPort oddness in Tesla Theater Mode.
 
 
   constructor(snack : MatSnackBar) {
+    this.isFullscreen = false;
+
     if (document.referrer != 'https://www.youtube.com/' &&
         navigator.userAgent.match(this.teslaUserAgentRegEx)) {
 
@@ -22,6 +25,9 @@ export class AppComponent {
         .subscribe(()=>{
           location.href = 'https://youtube.com/redirect?q=https://testube.app';
         })
+    } else if (navigator.userAgent.match(this.teslaUserAgentRegEx)) {
+      this.isFullscreen = true;
+      document.documentElement.setAttribute('style','zoom: '+ this.theaterZoomLevel);
     }
   }
 

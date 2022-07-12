73369a4a50d3aa415b65e3e8df678943b6e94731
JaxonWright <jaxonhwright@gmail.com>
2022-07-11 22:15:30 -0400

    debug: add "Report Tesla Detection Issue" button to report issue to Sentry for examination

--- src/app/pages/debug/debug.component.ts
+++ src/app/pages/debug/debug.component.ts
@@ -5,7 +5,7 @@ import {
 import {
   VERSION
 } from '../../../environments/version';
-import {Clipboard} from '@angular/cdk/clipboard';
+import * as Sentry from '@sentry/angular';
 @Component({
   selector: 'app-debug',
   templateUrl: './debug.component.html',
@@ -20,9 +20,7 @@ export class DebugComponent implements OnInit {
   minZoom: number;
   teslaUserAgentRegEx = /Tesla\/(?:develop-)*(?:feature-)*(?:.*fsd.*)*([0-9]{4}.[0-9]{1,3}.?[0-9]{0,3}.?[0-9]{0,3}.?[0-9]{0,3})*-(.*)/g;
 
-  constructor(
-    private clipboard: Clipboard
-  ) {}
+  constructor() {}
 
   ngOnInit() {
     this.buildDate = new Date(VERSION.date).toLocaleString('en-US',{ year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' });;
@@ -43,9 +41,9 @@ export class DebugComponent implements OnInit {
   }
 
   getTeslaFirmware(): string {
-    console.log(this.userAgent.match(this.teslaUserAgentRegEx))
     if (this.userAgent.includes('fsd-eap')) return 'FSD Beta';
-    return this.teslaUserAgentRegEx.exec(this.userAgent)[1] ? this.teslaUserAgentRegEx.exec(this.userAgent)[1] : 'Unknown';
+    let exec = this.teslaUserAgentRegEx.exec(this.userAgent);
+    return exec[1] ? exec[1] : 'Unknown';
   }
 
   getViewport(width: boolean): string {
@@ -57,7 +55,11 @@ export class DebugComponent implements OnInit {
     document.documentElement.setAttribute('style', 'zoom: ' + this.zoom)
   }
 
-  copyUserAgent() {
-    this.clipboard.copy(this.userAgent);
+  reportDetectionIssue() {
+    Sentry.captureException(new Error("Failed to Identify Tesla UserAgent"), {
+      extra: {
+        'userAgent': this.userAgent
+      }
+    })
   }
 }

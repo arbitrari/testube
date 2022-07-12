beb461910a5a41f09bb0a52e3ff3232c3bfd3e4a
JaxonWright <jaxonhwright@gmail.com>
2022-07-11 20:05:19 -0400

    fix tesla useragent detection closes #30

--- src/app/pages/debug/debug.component.ts
+++ src/app/pages/debug/debug.component.ts
@@ -18,7 +18,7 @@ export class DebugComponent implements OnInit {
   zoom: number;
   maxZoom: number;
   minZoom: number;
-  teslaUserAgentRegEx = /Tesla\/(?:develop-)*(?:.*fsd.*)*([0-9]{4}.[0-9]{1,3}.?[0-9]{0,3}.?[0-9]{0,3}.?[0-9]{0,3})*-(.*)/g;
+  teslaUserAgentRegEx = /Tesla\/(?:develop-)*(?:feature-)*(?:.*fsd.*)*([0-9]{4}.[0-9]{1,3}.?[0-9]{0,3}.?[0-9]{0,3}.?[0-9]{0,3})*-(.*)/g;
 
   constructor(
     private clipboard: Clipboard

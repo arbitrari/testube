ff926f886119efcaba645e8af70b03173c7fdd47
JaxonWright <jaxonhwright@gmail.com>
2022-12-21 16:40:16 -0500

    update to mdc material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './elements/footer/footer.component';
 import { LogoComponent } from './elements/logo/logo.component';
 import { CardComponent } from './elements/card/card.component';
-import { DonateComponent } from './pages/donate/donate.component';
 import { DebugComponent } from './pages/debug/debug.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
-import {MatLegacySnackBarModule as MatSnackBarModule} from '@angular/material/legacy-snack-bar';
-import {MatLegacyFormFieldModule as MatFormFieldModule} from '@angular/material/legacy-form-field';
-import {MatLegacyInputModule as MatInputModule} from '@angular/material/legacy-input';
-import {MatLegacySliderModule as MatSliderModule} from '@angular/material/legacy-slider';
-import {MatLegacyTooltipModule as MatTooltipModule} from '@angular/material/legacy-tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatIconModule} from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 
@@ -36,7 +35,6 @@ import { Router } from '@angular/router';
     FooterComponent,
     LogoComponent,
     CardComponent,
-    DonateComponent,
     DebugComponent,
   ],
   imports: [

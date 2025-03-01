5d36ab61c1d965d974466993812322b74d9135b4
Jaxon Wright <jaxonhwright@gmail.com>
2025-03-01 12:31:56 -0500

    dash: switch to new conditional block formatting

--- src/app/pages/dash/dash.component.ts
+++ src/app/pages/dash/dash.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { SourceManagerService } from 'src/app/services/source-manager/source-manager.service';
 import { Category } from 'src/app/services/source-manager/types';
-import { NgFor } from '@angular/common';
 import { CardComponent } from '../../elements/card/card.component';
 
 @Component({
     selector: 'app-dash',
     templateUrl: './dash.component.html',
     styleUrls: ['./dash.component.scss'],
-    imports: [NgFor, CardComponent]
+    imports: [ CardComponent]
 })
 export class DashComponent implements OnInit {
   categories : Category[];
